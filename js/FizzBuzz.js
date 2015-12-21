afc7e2732e43a290342bd2390b06ce7b68885337
hamauzunoboru <hamauzu.noboru@gmail.com>
2015-12-21 13:33:05 +0900

    Sampleプログラム作成

---
+++ js/FizzBuzz.js
@@ -0,0 +1,34 @@
+function FizzBuzz(value) {
+	this.maxValue = value;
+}
+
+function _doFizzBuzz() {
+	var test = "1";
+	var result;
+	for( var i = 2; i <= this.maxValue ; i++ ) {
+		result = this.getFizzBuzz(i);
+		test = test + "," + result;
+	}
+	return test;
+}
+FizzBuzz.prototype.doFizzBuzz = _doFizzBuzz;
+
+function _getFizzBuzz(value) {
+	var resultFizz = ( value % 3 == 0 );
+	var resultBuzz = ( value % 5 == 0 );
+	
+	if ( resultFizz && resultBuzz ) { return "Fizz Buzz"; }
+	if ( resultFizz ) { return "Fizz"; }
+	if ( resultBuzz ) { return "Buzz"; }
+	
+	return value;
+	
+	// return "1";
+}
+FizzBuzz.prototype.getFizzBuzz = _getFizzBuzz;
+
+function dispTest() {
+	var fizzBuzzTest = new FizzBuzz(30);
+	document.getElementById("test").innerHTML = fizzBuzzTest.doFizzBuzz();
+}
+
